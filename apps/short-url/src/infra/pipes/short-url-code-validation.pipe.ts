48086a73c5a357e261a1e22aeb8b4f35f30d175f
devdbreno <dbrenoti@gmail.com>
2026-03-19 19:53:02 -0300

    fix(short-url): accept "_" and "-" in short url code validation pipe

--- apps/short-url/src/infra/pipes/short-url-code-validation.pipe.ts
+++ apps/short-url/src/infra/pipes/short-url-code-validation.pipe.ts
@@ -2,7 +2,7 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class ShortUrlCodeValidationPipe implements PipeTransform {
-  private readonly regex = /^[a-zA-Z0-9]{8}$/;
+  private readonly regex = /^[a-zA-Z0-9_-]{8}$/;
 
   transform(value: string): string {
     if (!this.regex.test(value)) throw new BadRequestException('Invalid shortUrlCode format');
